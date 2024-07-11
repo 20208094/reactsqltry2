2f32d9066116f7995c0dfad045c10d1739bdc8dd
RIE ZHENZY ZUMEL <20208094@s.ubaguio.edu>
2024-07-11 17:50:58 +0800

    8082

--- backendtryonly/server.js
+++ backendtryonly/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8082;
 
 const config = {
     user: process.env.DB_USER || 'agritayo',
